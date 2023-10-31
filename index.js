01d63c15b2e1a4ad98dffcb48aad4db3b10e60ce
salman-farcy <salmanfarcy253@gmail.com>
2023-10-31 08:26:01 +0600

    Update/PATCH completed from server site

--- index.js
+++ index.js
@@ -79,6 +79,23 @@ async function run() {
       res.send(result)
     })
 
+    //Updete Put/patch
+    app.patch('/bookings/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedBooking = req.body
+      console.log(updatedBooking);
+
+      const updateDoc = {
+        $set:{
+          status: updatedBooking.status
+        }
+      }
+
+      const result = await booKingCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+
 
     //bookings delet
     app.delete('/bookings/:id', async(req, res) => {
